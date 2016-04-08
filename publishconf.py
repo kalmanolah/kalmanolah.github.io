3403d2cd5331a57b7a462024df66313228460e31
Kalman Olah <hello@kalmanolah.net>
2016-04-08 23:31:02 +0200

    wonder if I can do https
    Signed-off-by: Kalman Olah <hello@kalmanolah.net>

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *  # noqa
 
-SITEURL = 'http://kalmanolah.net'
+SITEURL = 'https://kalmanolah.net'
 FEED_DOMAIN = SITEURL
 
 DELETE_OUTPUT_DIRECTORY = True
